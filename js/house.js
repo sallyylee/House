ebdd3e6d8ce9a7fe41c637bd38689bc13056a54e
Sally Lee <sallyyleee@sallys-air.wifi.bcit.ca>
2016-11-12 20:25:15 -0800

    I put everything under the smoke.js file

--- js/house.js
+++ js/house.js
@@ -397,3 +397,94 @@ function drawFlower3() {
     ctx.strokeStyle = "#23681B";
     ctx.stroke();
 }
+
+window.onload = function() {
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+    
+    var slider = document.getElementById("slider");
+
+    var sliderVar = 0; //variable to be controlled
+
+    var dispDiv = document.getElementById("dispDiv");
+
+    //Slider
+    slider.addEventListener("change", function() { 
+      sliderVar = slider.value;  
+    })
+    setInterval(function() {
+      sliderVar = slider.value;
+    }, 100)
+	
+
+	var W = window.innerWidth, H = window.innerHeight;
+	canvas.width = 500;
+	canvas.height = 500;
+	
+	var particles = [];
+	
+	var particle_count = 75;
+	for(var i = 0; i < particle_count; i++)
+	{
+		particles.push(new particle());
+	}
+	function particle()
+	{
+		//speed, angle and spread
+		this.speed = {x: -2 + Math.random() * 5, y: -14 + Math.random() * 15};
+		
+        //location
+		this.location = {x: canvas.width/2, y: canvas.height/2};
+        
+		//range of radius
+		this.radius = 10 + Math.random() * 20;
+        
+		//range of the life
+		this.life = 20+Math.random()*10;
+		this.remaining_life = this.life;
+        
+		//color of smoke
+		this.r = 60;
+		this.g = 60;
+		this.b = 60;
+	}
+	
+	function draw()
+	{
+		ctx.globalCompositeOperation = "source-over";
+		ctx.fillStyle = "white";
+		ctx.fillRect(0, 0, W, H);
+		
+		for(var i = 0; i < particles.length; i++)
+		{
+			var p = particles[i];
+			ctx.beginPath();
+            
+            //change opacity of particles as they die
+			p.opacity = Math.round(p.remaining_life/p.life*100)/100
+            
+			//Gradient
+			var gradient = ctx.createRadialGradient(p.location.x, p.location.y, 0, p.location.x, p.location.y, p.radius);
+			gradient.addColorStop(0, "rgba("+p.r+", "+p.g+", "+p.b+", "+p.opacity+")");
+			gradient.addColorStop(0.5, "rgba("+p.r+", "+p.g+", "+p.b+", "+p.opacity+")");
+			gradient.addColorStop(1, "rgba("+p.r+", "+p.g+", "+p.b+", 0)");
+			ctx.fillStyle = gradient;
+			ctx.arc(p.location.x, p.location.y, p.radius, Math.PI*2, false);
+			ctx.fill();
+			
+			//move particles.
+			p.remaining_life--;
+			p.radius--;
+			p.location.x += p.speed.x;
+			p.location.y += p.speed.y - sliderVar/5;
+			
+			//make particle when another one dies
+			if(p.remaining_life < 0 || p.radius < 0)
+			{
+				particles[i] = new particle();
+			}
+		}
+	}
+	
+	setInterval(draw, 33);
+}
